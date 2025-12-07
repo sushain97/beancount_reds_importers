1aec9f5c31c40692371986d95d2d63a75d396e77
thehill <register@washdcmail.com>
2025-12-06 19:40:01 -0500

    fix core_acct_balance needs to be a Decimal

--- beancount_reds_importers/importers/fidelity/fidelity_all_account_positions_csv.py
+++ beancount_reds_importers/importers/fidelity/fidelity_all_account_positions_csv.py
@@ -2,6 +2,7 @@
 
 import datetime
 import re
+from decimal import Decimal
 
 from beancount.core.number import D
 
@@ -126,11 +127,7 @@ class Importer(investments.Importer, csvreader.Importer):
                         yield pos
 
     def get_available_cash(self, settlement_fund_balance):
-        # if this is returned as 0 then
-        # amount.Amount(available_cash, self.currency) fails, but setting this
-        # to none will mean we do not have $0 balance assertions when they should
-        # exist
-        core_acct_balance = None
+        core_acct_balance = Decimal()
         for pos in self.rdr.namedtuples():
             if pos.account_number == self.config["account_number"]:
                 if pos.security.endswith("**"):  # these are core (cash) accounts
