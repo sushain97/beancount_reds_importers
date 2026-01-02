580cdc649d2c3b08a5f7387dde6f6ac067265a65
thehill <register@washdcmail.com>
2026-01-02 11:23:24 -0500

    feat: update test to use custom column labels and strip out ($)

--- beancount_reds_importers/importers/fidelity/tests/fidelity_all_accounts_csv/fidelity_all_accounts_csv_test.py
+++ beancount_reds_importers/importers/fidelity/tests/fidelity_all_accounts_csv/fidelity_all_accounts_csv_test.py
@@ -97,6 +97,7 @@ config = {
     "fund_info": fund_info,
     "emit_filing_account_metadata": False,
     "filename_pattern": "fidelity_csv_all_accounts_transactions_.*.csv",
+    "column_labels_line": "Run Date,Account,Account Number,Action,Symbol,Description,Type,Exchange Quantity,Exchange Currency,Currency,Price,Quantity,Exchange Rate,Commission,Fees,Accrued Interest,Amount ($),Settlement Date",
 }
 
 # print(config)
